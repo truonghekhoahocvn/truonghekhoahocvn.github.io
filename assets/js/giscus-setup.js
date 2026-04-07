d33a91340d2f44e27f90718ce5a2d654363d1623
truonghekhoahocvietnam <trung@hust.org.vn>
2026-04-07 16:51:58 +0000

    Deploying to gh-pages from @ truonghekhoahocvn/truonghekhoahocvn.github.io@ff5a06ed2da5cd64602274adfe2bd774f849634e 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "truonghekhoahocvietnam/truonghekhoahocvietnam.github.io",
+    "data-repo": "truonghekhoahocvn/truonghekhoahocvn.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
